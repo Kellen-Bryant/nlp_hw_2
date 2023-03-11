1c27c18a2c44933d9921427c93be7c74ce029bab
rreichert01 <rodneyreichert2024@u.northwestern.edu>
2023-03-11 14:21:41 -0600

    Implemented validation loop

--- starter.py
+++ starter.py
@@ -9,15 +9,15 @@ import json
 import numpy as np
 
 
-def main():  
+def main():
     torch.manual_seed(0)
-    answers = ['A','B','C','D']
+    answers = ['A', 'B', 'C', 'D']
 
     train = []
     test = []
     valid = []
-    
-    file_name = 'train_complete.jsonl'        
+
+    file_name = 'train_complete.jsonl'
     with open(file_name) as json_file:
         json_list = list(json_file)
     for i in range(len(json_list)):
@@ -26,7 +26,7 @@ def main():
         # print(result)
         base = result['fact1'] + ' [SEP] ' + result['question']['stem']
         ans = answers.index(result['answerKey'])
-        
+
         obs = []
         for j in range(4):
             text = base + result['question']['choices'][j]['text'] + ' [SEP]'
@@ -34,9 +34,9 @@ def main():
                 label = 1
             else:
                 label = 0
-            obs.append([text,label])
+            obs.append([text, label])
         train.append(obs)
-        
+
         # print(obs)
         # print(' ')
         #
@@ -49,17 +49,17 @@ def main():
         # print('  Fact: ',result['fact1'])
         # print('  Answer: ',result['answerKey'])
         # print('  ')
-                
-    file_name = 'dev_complete.jsonl'        
+
+    file_name = 'dev_complete.jsonl'
     with open(file_name) as json_file:
         json_list = list(json_file)
     for i in range(len(json_list)):
         json_str = json_list[i]
         result = json.loads(json_str)
-        
+
         base = result['fact1'] + ' [SEP] ' + result['question']['stem']
         ans = answers.index(result['answerKey'])
-        
+
         obs = []
         for j in range(4):
             text = base + result['question']['choices'][j]['text'] + ' [SEP]'
@@ -67,19 +67,19 @@ def main():
                 label = 1
             else:
                 label = 0
-            obs.append([text,label])
+            obs.append([text, label])
         valid.append(obs)
-        
-    file_name = 'test_complete.jsonl'        
+
+    file_name = 'test_complete.jsonl'
     with open(file_name) as json_file:
         json_list = list(json_file)
     for i in range(len(json_list)):
         json_str = json_list[i]
         result = json.loads(json_str)
-        
+
         base = result['fact1'] + ' [SEP] ' + result['question']['stem']
         ans = answers.index(result['answerKey'])
-        
+
         obs = []
         for j in range(4):
             text = base + result['question']['choices'][j]['text'] + ' [SEP]'
@@ -87,7 +87,7 @@ def main():
                 label = 1
             else:
                 label = 0
-            obs.append([text,label])
+            obs.append([text, label])
         test.append(obs)
 
     tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
@@ -97,46 +97,64 @@ def main():
     optimizer = optim.Adam([{"params": model.parameters()}, {"params": linear.parameters()}], lr=3e-5)
     # linear = torch.rand(768,2)
 
-    
     #    Add code to fine-tune and test your MCQA classifier.
 
     epochs = 3
     batch_size = 64
 
     # Init Training data loader
-    input_x = torch.zeros(len(train)*4, 128, dtype=torch.int)
-    input_y = torch.zeros(len(train)*4)
+    input_x = torch.zeros(len(train) * 4, 128, dtype=torch.int)
+    input_y = torch.zeros(len(train) * 4)
     for idx_q, question in enumerate(train):
         for idx_a, answer in enumerate(question):
-            input_x[idx_q*4 + idx_a] = torch.tensor(tokenizer(answer[0], padding="max_length", truncation=True, max_length=128)["input_ids"], dtype=torch.int)
-            input_y[idx_q*4 + idx_a] = answer[1]
+            input_x[idx_q * 4 + idx_a] = torch.tensor(
+                tokenizer(answer[0], padding="max_length", truncation=True, max_length=128)["input_ids"],
+                dtype=torch.int)
+            input_y[idx_q * 4 + idx_a] = answer[1]
     train_dataset = torch.utils.data.TensorDataset(input_x, input_y)
     train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size)
 
     # init val set
-    val_input_x = torch.zeros(len(valid), 4, 128, dtype=torch.int)
-    val_input_y = torch.zeros(len(valid)*4)
+    val_input_x = torch.zeros(len(valid) * 4, 128, dtype=torch.int)
+    val_input_y = torch.zeros(len(valid) * 4)
     for idx_q, question in enumerate(valid):
         for idx_a, answer in enumerate(question):
-            input_x[idx_q, idx_a] = torch.tensor(tokenizer(answer[0], padding="max_length", truncation=True, max_length=128)["input_ids"], dtype=torch.int)
-            input_y[idx_q, idx_a] = answer[1]
+            val_input_x[idx_q * 4 + idx_a] = torch.tensor(
+                tokenizer(answer[0], padding="max_length", truncation=True, max_length=128)["input_ids"],
+                dtype=torch.int)
+            val_input_y[idx_q * 4 + idx_a] = answer[1]
     val_dataset = torch.utils.data.TensorDataset(val_input_x, val_input_y)
-    val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size)
+    val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=4)
+    # for bat, (X, y) in enumerate(val_dataloader):
+    #     print(X, y)
+    #     input("")
 
     for epoch in range(epochs):
-        for batch_idx, (X, y) in enumerate(train_dataloader):
-            model.train()
-            optimizer.zero_grad()
-            BERT_out = model(X)[1]
-            lin_out = linear(BERT_out)
-            # print(lin_out)
-            preds = torch.nn.Sigmoid()(lin_out).squeeze()
-            # print(preds)
-            loss = torch.nn.BCELoss()(preds, y)
-            print(loss)
-            loss.backward()
-            # torch.nn.utils.clip_grad_norm_(model.parameters(), 1)
-            optimizer.step()
+        # for batch_idx, (X, y) in enumerate(train_dataloader):
+        #     model.train()
+        #     optimizer.zero_grad()
+        #     BERT_out = model(X)[1]
+        #     lin_out = linear(BERT_out)
+        #     # print(lin_out)
+        #     preds = torch.nn.Sigmoid()(lin_out).squeeze()
+        #     # print(preds)
+        #     loss = torch.nn.BCELoss()(preds, y)
+        #     print(loss)
+        #     loss.backward()
+        #     # torch.nn.utils.clip_grad_norm_(model.parameters(), 1)
+        #     optimizer.step()
+        for question_idx, (X, y) in enumerate(val_dataloader):
+            model.eval()
+            with torch.no_grad():
+                BERT_out = model(X)[1]
+                lin_out = linear(BERT_out)
+                # print(lin_out)
+                preds = torch.nn.Sigmoid()(lin_out).squeeze()
+                if torch.argmax(preds).item() == torch.argmax(y).item():
+                    print('correct')
+                # print(torch.argmax(preds), torch.argmax(y))
+                # print(preds, y)
+
 
 if __name__ == "__main__":
     main()
