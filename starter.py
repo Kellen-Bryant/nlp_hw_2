da6f038850d9f1befd099f6fd9bcd805316d58da
rreichert01 <rodneyreichert2024@u.northwestern.edu>
2023-03-13 10:50:28 -0500

    Implemented classification approach

--- starter.py
+++ starter.py
@@ -1,23 +1,24 @@
 from transformers import AutoTokenizer, BertModel, GPT2LMHeadModel, GPT2Tokenizer
 import torch.optim as optim
 
-import torch
 import math
 import time
 import sys
 import json
 import numpy as np
+import torch
 
 
 def main():
-    torch.manual_seed(0)
     answers = ['A', 'B', 'C', 'D']
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    display(device)
 
     train = []
     test = []
     valid = []
 
-    file_name = 'train_complete.jsonl'
+    file_name = '/kaggle/input/mcqa-dataset-cs497/test_complete.jsonl'
     with open(file_name) as json_file:
         json_list = list(json_file)
     for i in range(len(json_list)):
@@ -50,7 +51,7 @@ def main():
         # print('  Answer: ',result['answerKey'])
         # print('  ')
 
-    file_name = 'dev_complete.jsonl'
+    file_name = '/kaggle/input/mcqa-dataset-cs497/dev_complete.jsonl'
     with open(file_name) as json_file:
         json_list = list(json_file)
     for i in range(len(json_list)):
@@ -70,7 +71,7 @@ def main():
             obs.append([text, label])
         valid.append(obs)
 
-    file_name = 'test_complete.jsonl'
+    file_name = '/kaggle/input/mcqa-dataset-cs497/train_complete.jsonl'
     with open(file_name) as json_file:
         json_list = list(json_file)
     for i in range(len(json_list)):
@@ -89,18 +90,24 @@ def main():
                 label = 0
             obs.append([text, label])
         test.append(obs)
-
+    best_pretrain_acc = 0
+    #     for model_lr in [1e-5, 3e-5, 7e-5]:
+    #         for lin_lr in [1e-5, 3e-5, 7e-5]:
+    torch.manual_seed(0)
     tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
-    model = BertModel.from_pretrained("bert-base-uncased")
+    model = BertModel.from_pretrained("bert-base-uncased").to(device)
     optimizer = optim.Adam(model.parameters(), lr=3e-5)
-    linear = torch.nn.Linear(768, 1)
-    optimizer = optim.Adam([{"params": model.parameters()}, {"params": linear.parameters()}], lr=3e-5)
-    # linear = torch.rand(768,2)
+    linear = torch.nn.Linear(768, 1).to(device)
+    optimizer = optim.Adam([{"params": model.parameters()}, {"params": linear.parameters(), 'lr': 3e-5}], lr=3e-5)
+    #     optimizer = optim.Adam(linear.parameters(), lr=3e-5)
+    #     optimizer = optim.Adam(model.parameters(), lr=3e-5)
+    #     linear = torch.rand(768,1)
 
     #    Add code to fine-tune and test your MCQA classifier.
 
-    epochs = 3
+    epochs = 6
     batch_size = 64
+    zero_shot = False
 
     # Init Training data loader
     input_x = torch.zeros(len(train) * 4, 128, dtype=torch.int)
@@ -111,7 +118,7 @@ def main():
                 tokenizer(answer[0], padding="max_length", truncation=True, max_length=128)["input_ids"],
                 dtype=torch.int)
             input_y[idx_q * 4 + idx_a] = answer[1]
-    train_dataset = torch.utils.data.TensorDataset(input_x, input_y)
+    train_dataset = torch.utils.data.TensorDataset(input_x.to(device), input_y.to(device))
     train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size)
 
     # init val set
@@ -123,37 +130,77 @@ def main():
                 tokenizer(answer[0], padding="max_length", truncation=True, max_length=128)["input_ids"],
                 dtype=torch.int)
             val_input_y[idx_q * 4 + idx_a] = answer[1]
-    val_dataset = torch.utils.data.TensorDataset(val_input_x, val_input_y)
+    val_dataset = torch.utils.data.TensorDataset(val_input_x.to(device), val_input_y.to(device))
     val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=4)
-    # for bat, (X, y) in enumerate(val_dataloader):
-    #     print(X, y)
-    #     input("")
 
+    # init test set
+    test_input_x = torch.zeros(len(test) * 4, 128, dtype=torch.int)
+    test_input_y = torch.zeros(len(test) * 4)
+    for idx_q, question in enumerate(test):
+        for idx_a, answer in enumerate(question):
+            test_input_x[idx_q * 4 + idx_a] = torch.tensor(
+                tokenizer(answer[0], padding="max_length", truncation=True, max_length=128)["input_ids"],
+                dtype=torch.int)
+            test_input_y[idx_q * 4 + idx_a] = answer[1]
+    test_dataset = torch.utils.data.TensorDataset(test_input_x.to(device), test_input_y.to(device))
+    test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=4)
+
+    acc = 0
+    for question_idx, (X, y) in enumerate(val_dataloader):
+        model.eval()
+        with torch.no_grad():
+            BERT_out = model(X)[1]
+            lin_out = linear(BERT_out)
+            preds = torch.nn.Sigmoid()(lin_out).squeeze()
+            if torch.argmax(preds).item() == torch.argmax(y).item():
+                acc += 1
+    print(f"Zero Shot val Accuracy:{acc / len(valid)}")
+    acc = 0
+    for question_idx, (X, y) in enumerate(test_dataloader):
+        model.eval()
+        with torch.no_grad():
+            BERT_out = model(X)[1]
+            lin_out = linear(BERT_out)
+            preds = torch.nn.Sigmoid()(lin_out).squeeze()
+            if torch.argmax(preds).item() == torch.argmax(y).item():
+                acc += 1
+    print(f"Zero Shot test Accuracy:{acc / len(test)}")
+
+    #             print(len(valid))
     for epoch in range(epochs):
-        # for batch_idx, (X, y) in enumerate(train_dataloader):
-        #     model.train()
-        #     optimizer.zero_grad()
-        #     BERT_out = model(X)[1]
-        #     lin_out = linear(BERT_out)
-        #     # print(lin_out)
-        #     preds = torch.nn.Sigmoid()(lin_out).squeeze()
-        #     # print(preds)
-        #     loss = torch.nn.BCELoss()(preds, y)
-        #     print(loss)
-        #     loss.backward()
-        #     # torch.nn.utils.clip_grad_norm_(model.parameters(), 1)
-        #     optimizer.step()
+        for batch_idx, (X, y) in enumerate(train_dataloader):
+            model.train()
+            optimizer.zero_grad()
+            BERT_out = model(X)[1]
+            lin_out = linear(BERT_out)
+            # print(lin_out)
+            preds = torch.nn.Sigmoid()(lin_out).squeeze()
+            # print(preds)
+            loss = torch.nn.BCELoss()(preds, y)
+            #                 print(loss)
+            loss.backward()
+            # torch.nn.utils.clip_grad_norm_(model.parameters(), 1)
+            optimizer.step()
+        acc = 0
         for question_idx, (X, y) in enumerate(val_dataloader):
             model.eval()
             with torch.no_grad():
                 BERT_out = model(X)[1]
                 lin_out = linear(BERT_out)
-                # print(lin_out)
                 preds = torch.nn.Sigmoid()(lin_out).squeeze()
                 if torch.argmax(preds).item() == torch.argmax(y).item():
-                    print('correct')
-                # print(torch.argmax(preds), torch.argmax(y))
-                # print(preds, y)
+                    acc += 1
+        print(f"Val Accuracy:{acc / len(valid)}")
+    acc = 0
+    for question_idx, (X, y) in enumerate(test_dataloader):
+        model.eval()
+        with torch.no_grad():
+            BERT_out = model(X)[1]
+            lin_out = linear(BERT_out)
+            preds = torch.nn.Sigmoid()(lin_out).squeeze()
+            if torch.argmax(preds).item() == torch.argmax(y).item():
+                acc += 1
+    print(f"Test Accuracy:{acc / len(test)}")
 
 
 if __name__ == "__main__":
